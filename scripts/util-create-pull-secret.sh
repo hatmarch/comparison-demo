6c48611008a7b45c03c22db1caa6a572a42e55a1
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-03-17 02:42:25 +0000

    Add utility script for creating pull secrets
    This is necessary to pull the s2i images in the way the new tomcat-s2i task is setup.

---
+++ scripts/util-create-pull-secret.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USER="$1"
+PASSWORD="$2"
+SECRET_NAME=$3
+PROJECT_NAME=$4
+
+if [ -z "$USER" ]; then
+    echo "Must specify a user for registry.redhat.io"
+    exit 1
+fi
+
+if [ -z "$PASSWORD" ]; then
+    echo "Must specify a password for registry.redhat.io"
+    exit 1
+fi
+
+if [ -z "${SECRET_NAME}" ]; then
+    SECRET_NAME="redhat-registry-pull"
+fi
+
+if [ -z "${PROJECT_NAME}" ]; then
+    # use the current project
+    PROJECT_NAME=$(oc project -q)
+fi
+
+echo "Using user: $USER and password: $PASSWORD for secret $SECRET_NAME in project ${PROJECT_NAME}"
+
+# Create a pull secret in the current project redhat.registry.io 
+oc create secret docker-registry $SECRET_NAME \
+    --docker-server=registry.redhat.io \
+    --docker-username="$USER" \
+    --docker-password="$PASSWORD" \
+    --docker-email=mhildenb@redhat.com -n $PROJECT_NAME
+
+# This should supposedly use --for=pull as its a pull secret, however in OpenShift 4.3 that doesn't appear to work exclusively
+oc secrets link pipeline $SECRET_NAME -n $PROJECT_NAME --for=pull
+oc secrets link pipeline $SECRET_NAME -n $PROJECT_NAME 
+
+
