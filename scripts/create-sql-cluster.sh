2dce942c2bbf579b11188bfd14ec33c910b91cc5
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-03-18 07:32:08 +0000

    Add support for creating and deploying to a staging environment.
    Add template for deploying a staging environment
    
    Adds a script for all aspects of creating a SQL cluster in a specifc project
    
    Shows how to disable caching to test image updates

---
+++ scripts/create-sql-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+oc apply -f $DEMO_HOME/kube/operators/mysql/mysql-agent.yaml
+oc adm policy add-scc-to-user anyuid -z mysql-agent 
+
+oc create secret generic mysql-root-password --from-literal=password="password"
+
+oc apply -f $DEMO_HOME/kube/operators/mysql/mysql-cluster-instance.yaml
+oc apply -f $DEMO_HOME/kube/operators/mysql/mysql-primary-service.yaml
+
+# wait for all pods in the cluster to be ready (loop on this as pods are not)
+# created all at once
+oc wait --for=condition=ready pod -l v1alpha1.mysql.oracle.com/cluster=jws-app-mysql-cluster --timeout=5m
+sleep 5
+oc wait --for=condition=ready pod -l v1alpha1.mysql.oracle.com/cluster=jws-app-mysql-cluster --timeout=5m
+sleep 5
+oc wait --for=condition=ready pod -l v1alpha1.mysql.oracle.com/cluster=jws-app-mysql-cluster --timeout=5m
+
+# create the database and user for our service to use
+oc run mysql-client --image=mysql:5.7 --restart=Never --rm=true --attach=true --wait=true \
+    -- mysql -h jws-app-mysql -uroot -ppassword -e "CREATE USER 'pc'@'%' IDENTIFIED BY 'petclinic'; \
+      CREATE DATABASE petclinic; \
+      GRANT ALL PRIVILEGES ON petclinic.* TO 'pc'@'%';"
